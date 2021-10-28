139fadb209ba6bb8ce91b00e15c54dd225deb48c
jeanrira <jeanrira@campusaula.com>
2021-10-28 14:29:51 +0200

    commit

---
+++ PHP/Programa_Mayor.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programa Mayor</title>
+</head>
+<body>
+    <?php
+    $var1 = 1;
+    $var2 = 2;
+    $var3 = 3;
+
+    if ($var1 > $var2 && $var1 > $var3) {
+        echo "el mayor valor es $var1";
+    }
+
+    ?>
+</body>
+</html>
