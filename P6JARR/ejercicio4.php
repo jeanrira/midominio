488d460dce7f2a92aafdd3619fcb6f19a30bec2a
jeanrira <jeanrira@campusaula.com>
2021-11-11 14:21:42 +0100

    commit

---
+++ P6JARR/ejercicio4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <?php
+    $num = 2;
+    echo "Tabla del 2"."<br/>";
+    echo "**********"."<br/>";
+    echo "2 x 1 = ".$num*1 ."<br/>";
+    echo "2 x 2 = ".$num*2 ."<br/>";
+    echo "2 x 3 = ".$num*3 ."<br/>";
+    echo "2 x 4 = ".$num*4 ."<br/>";
+    echo "2 x 5 = ".$num*5 ."<br/>";
+    echo "2 x 6 = ".$num*6 ."<br/>";
+    echo "2 x 7 = ".$num*7 ."<br/>";
+    echo "2 x 8 = ".$num*8 ."<br/>";
+    echo "2 x 9 = ".$num*9 ."<br/>";
+    echo "2 x 10 = ".$num*10 ."<br/>";
+    echo "........";
+    ?>
+</body>
+</html>
